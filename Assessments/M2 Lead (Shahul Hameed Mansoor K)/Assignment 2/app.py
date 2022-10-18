bd0cd09ebd5f67d33c54c682ae4c4d1488bb2e76
Marx <shahulhameedmansoork@gmail.com>
2022-10-18 20:46:53 +0530

    Assignt pdf uploaded

--- Assessments/M2 Lead (Shahul Hameed Mansoor K)/Assignment 2/app.py
+++ Assessments/M2 Lead (Shahul Hameed Mansoor K)/Assignment 2/app.py
@@ -1,8 +1,13 @@
 from re import T
-from flask import Flask,render_template,redirect,url_for
+from flask import Flask,render_template,flash,redirect,url_for,session,request
+import sqlite3
 
 app=Flask(__name__)
+app.secret_key="123"
 
+con=sqlite3.connect("database.db")
+con.execute("create table if not exists customer(pid integer primary key,name text,address text,contact integer,mail text)")
+con.close()
 
 @app.route("/")
 def homePage():
@@ -12,13 +17,44 @@ def homePage():
 def aboutPage():
     return "about Page !!!"
 
-@app.route("/signin")
+@app.route("/signin",methods=["GET","POST"])
 def signinPAGE():
+    if request.method=='POST':
+        name=request.form['name']
+        password=request.form['password']
+        con=sqlite3.connect("database.db")
+        con.row_factory=sqlite3.Row
+        cur=con.cursor()
+        cur.execute("select * from customer where name=? and mail=?",(name,password))
+        data=cur.fetchone()
+
+        if data:
+            session["name"]=data["name"]
+            session["mail"]=data["mail"]
+            return redirect("home")
+        else:
+            flash("Username and Password Mismatch","danger")
     return render_template('signin.html')
 
 
-@app.route("/signup")
+@app.route("/signup",methods=['GET','POST'])
 def signupPAGE():
+    if request.method=='POST':
+        try:
+            name=request.form['name']
+            address=request.form['address']
+            contact=request.form['contact']
+            mail=request.form['mail']
+            con=sqlite3.connect("database.db")
+            cur=con.cursor()
+            cur.execute("insert into customer(name,address,contact,mail)values(?,?,?,?)",(name,address,contact,mail))
+            con.commit()
+            flash("Record Added  Successfully","success")
+        except:
+            flash("Error in Insert Operation","danger")
+        finally:
+            return redirect(url_for("home"))
+            con.close()
     return render_template('signup.html')
 
 
