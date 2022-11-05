225f868e273671a896cf117ccd74215e80d66d0c
siva29631 <sivasubramanian29631@gmail.com>
2022-11-05 10:30:19 +0530

    add-siva : assign 2

--- Assessments/Team Lead(Siva Subramanian S)/Assignment 2/sqlite_setup_db.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-conn=sqlite3.connect('users.db')
-print('Database opened successfully')
-
-conn.execute('CREATE TABLE USERS (name TEXT,address TEXT,city TEXT, pin NUMBER)')
-print('Table created successfully')
-conn.close()
